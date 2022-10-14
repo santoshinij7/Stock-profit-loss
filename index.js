98d15e9e3f0d6b0fa1d9201ebf09987185072293
Santoshini Jena <83078159+santoshinij7@users.noreply.github.com>
2022-10-14 09:54:48 +0530

    update index.js

--- index.js
+++ index.js
@@ -5,23 +5,26 @@ const checkBtn = document.querySelector("#check-btn");
 const outputMsg = document.querySelector("#output");
 
 function calculateProfitAndLoss(initial, quantity, current) {
-    if (initial > current) {
-        var loss = (initial - current) * quantity;
-        var lossPercentage = (loss / (initial * quantity)) * 100;
+    if (initial > 0 && quantity > 0 && current > 0) {
+        if (initial > current) {
+            var loss = (initial - current) * quantity;
+            var lossPercentage = (loss / (initial * quantity)) * 100;
 
-        msgColor("red");
+            msgColor("red");
+            showMsg(`oh! ohh! Your total Loss is ${loss} by ${lossPercentage.toFixed(2)}%.`);
+        } else if (initial < current) {
+            var profit = (current - initial) * quantity;
+            var profitPercentage = (profit / (initial * quantity)) * 100;
 
-        showMsg(`OH OH! Your total Loss is ${loss} by ${lossPercentage.toFixed(2)}%.`);
-    } else if (initial < current) {
-        var profit = (current - initial) * quantity;
-        var profitPercentage = (profit / (initial * quantity)) * 100;
-
-        msgColor("green");
-
-        showMsg(`Yeah! Your total Profit is ${profit} by ${profitPercentage.toFixed(2)}%.`);
+            msgColor("green");
+            showMsg(`Yeah! Your total Profit is ${profit} by ${profitPercentage.toFixed(2)}%.`);
+        } else {
+            msgColor("black");
+            showMsg(`No Profit No Loss!!`);
+        }
     } else {
-        msgColor("black");
-        showMsg(`No Profit No Loss!!`);
+        msgColor("blue");
+        showMsg(`Input Value can't be Negative or Zero!!`);
     }
 }
 
